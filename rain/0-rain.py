40843918583dd31acac59ac3396815568f12902c
Philippa <p.giibwa@alustudent.com>
2024-02-06 12:36:28 +0000

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+interview rain"""
+
+
+def rain(walls):
+    if len(walls) == 0:
+        return 0
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left <= right:
+        if walls[left] <= walls[right]:
+            if walls[left] > left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] > right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
